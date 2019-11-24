8221d5f5491822f6d752ad004751eba349efcacb
unknown <you@example.com>
2019-11-24 16:32:48 +0530

    added feature to export results to text file

--- sumy-summarizer1.py
+++ sumy-summarizer1.py
@@ -11,5 +11,8 @@ parser = PlaintextParser.from_file(txtfile, Tokenizer("english"))
 summarizer = LexRankSummarizer()
 summary = summarizer(parser.document, 20)
 
-for sentence in summary:
-    print(sentence)
+
+with open('donaldtweetsummary' + '.txt', 'w') as txtfile:
+	for sentence in summary:
+        txtfile.write(sentence)
+        #txtfile.write(str(st.encode('utf-8')))
