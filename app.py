7e13dccfffe757bfa69380a77a3c08b02e4f370a
unknown <you@example.com>
2019-11-29 01:19:18 +0530

    soham is a bloody genius

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def landingpage():
 	choice = request.args.get('outtype')
 	text = request.args.get('text')
 	count = request.args.get('count')
-	if not text or not text.trim():
+	if not text or not text.strip():
 		return render_template("noinput.html")
 	if choice == 'wc':
 		return wordcloud(text)
